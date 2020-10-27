88d8809502eca771ab8e5a71e00775ae41722cf3
XiaoBei <learnroad@163.com>
2020-10-27 17:02:52 +0800

    'test'

---
+++ balanceRecord/main.go
@@ -0,0 +1,15 @@
+package main
+
+import (
+	"fmt"
+)
+
+// 编写家庭收支软件，实现记录收入与支出、退出的功能
+func main() {
+	fmt.Println("---------------------家庭收支软件-------------------")
+	fmt.Println("                      1.收支明细")
+	fmt.Println("                      2.登记收入")
+	fmt.Println("                      3.登记支出")
+	fmt.Println("                      4.退出软件")
+	fmt.Println("请选择功能（1-4）：")
+}
